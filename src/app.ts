b75cc3969a22a1dad023fb70b6c960705d7b6197
szabozsolt20 <szabo.zsolt.debrecen@gmail.com>
2022-01-07 02:55:45 +0100

    Itt átmegy a teszten

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 // Importáld be a HumanHero és TransformerHero osztályokat.
+import { HumanHero } from './hero';
+import { TransformerHero } from './transformer';
 
 
 /**
@@ -8,7 +10,11 @@
  * Hozz létre benne 3 HumanHero példányt, szabadon választott adatokkal.
  * @var {HumanHero[]} humans
  */
-export const humans: any[] = [];
+export const humans: HumanHero[] = [
+{id: 0, name: "string", sex: "string", photo: "string", age: 0, health: 0},
+{id: 1, name: "string", sex: "string", photo: "string", age: 0, health: 0},
+{id: 2, name: "string", sex: "string", photo: "string", age: 0, health: 0}
+];
 
 /**
  * Állítsd be helyesen a változót!
@@ -17,4 +23,8 @@ export const humans: any[] = [];
  * Hozz létre benne 3 TransformerHero példányt, szabadon választott adatokkal.
  * @var {TransformerHero[]} transformers
  */
-export const transformers: any[] = [];
+export const transformers: TransformerHero[] = [
+  {id: 0, name: "string", photo: "string", wings: 0, wheels: 0, clan: "string"},
+  {id: 1, name: "string", photo: "string", wings: 0, wheels: 0, clan: "string"},
+  {id: 2, name: "string", photo: "string", wings: 0, wheels: 0, clan: "string"},
+];
--- src/transformer.ts
+++ src/transformer.ts
@@ -7,6 +7,22 @@ import { ITransformer } from './hero-interface';
  * Deklaráld benne az interfész által megkövetelt változókat.
  * Definiáld a konstruktort és kérj be minden változót, majd állítsd be őket.
  */
-export class TransformerHero {
-    
+export class TransformerHero implements ITransformer {
+  wings: number;
+  wheels: number;
+  clan: string;
+
+  id: number;
+  name: string;
+  photo?: string;
+
+  constructor(id: number, name: string, wings: number, wheels: number, clan: string, photo?: string) {
+    this.id = id;
+    this.name = name;
+    this.photo = photo;
+    this.wings = wings;
+    this.wheels = wheels;
+    this.clan = clan;
+  }
+
 }
